1292d32304017e884086df63b434ca6592c9f4fa
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-09-23 15:43:58 +0700

    Ep-9-Basic-Routing

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  let fileName;
+  switch (req.url) {
+    case "/":
+      fileName = "index.html";
+      break;
+    case "/contact":
+      fileName = "contact.html";
+      break;
+    case "/about":
+      fileName = "about.html";
+      break;
+    default:
+      fileName = "404.html";
+      break;
+  }
+
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile("./views/" + fileName, (err, data) => {
     if (err) {
       console.log(err);
       res.end();

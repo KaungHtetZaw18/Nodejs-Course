8c84c6cfd42341f70fb5f014d88861518b9a6c60
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-09-23 14:26:29 +0700

    Ep-4-Node-Module-System

---
+++ module1.js
@@ -0,0 +1,5 @@
+const myName = "Kaung Htet Zaw";
+const myAge = 23;
+const another = "one";
+
+module.exports = { myName, myAge, another };
---
+++ module2.js
@@ -0,0 +1,4 @@
+const { myName, myAge, another } = require("./module1");
+
+console.log(myName, myAge);
+console.log(another);

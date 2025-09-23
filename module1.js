b28c80856be4111a13222f6ccb2f32e3e64e58ba
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-09-23 15:28:26 +0700

    Ep-8-render-html-file-to-client-from-node

--- module1.js
+++
@@ -1,5 +0,0 @@
-const myName = "Kaung Htet Zaw";
-const myAge = 23;
-const another = "one";
-
-module.exports = { myName, myAge, another };
--- module2.js
+++
@@ -1,4 +0,0 @@
-const { myName, myAge, another } = require("./module1");
-
-console.log(myName, myAge);
-console.log(another);

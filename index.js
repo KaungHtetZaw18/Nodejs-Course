03d8890c6d53f9a99f82e0c58da26f9f7ae913b8
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-09-23 14:02:13 +0700

    Ep-3-Global-Object

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+// let interval = global.setInterval(() => {
+//   console.log("Hello");
+// }, 1000);
+
+// global.setTimeout(() => {
+//   clearInterval(interval);
+// }, 5000);
+
+// console.log(global.document);
+
+console.log(__filename);

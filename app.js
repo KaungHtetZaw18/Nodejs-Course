587e0da90368236072300bf03b353140c0df7ae7
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-09-24 12:17:19 +0700

    Ep-17-Ejs-Pass-data-and-render-dynamic-contents

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  let blogs = [
+    { title: "Blog title 1", intro: "This is blog intro 1" },
+    { title: "Blog title 2", intro: "This is blog intro 2" },
+    { title: "Blog title 3", intro: "This is blog intro 3" },
+  ];
+  res.render("home", { blogs });
 });
 
 app.get("/about", (req, res) => {

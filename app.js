fef3a668fe36893364d853022763cb0901b4d0c3
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-09-24 17:55:56 +0700

    Ep-27-Get-all-blogs-and-single-blog

--- app.js
+++ app.js
@@ -28,20 +28,22 @@ app.use(express.static("public"));
 
 app.get("/add-blog", async (req, res) => {
   let blog = new Blog({
-    title: "blog title 2",
-    intro: "blog intro 2",
-    body: "blog body 2",
+    title: "blog title 3",
+    intro: "blog intro 3",
+    body: "blog body 3",
   });
   await blog.save();
   res.send("blog saved");
 });
 
-app.get("/", (req, res) => {
-  let blogs = [
-    { title: "Blog title 1", intro: "This is blog intro 1" },
-    { title: "Blog title 2", intro: "This is blog intro 2" },
-    { title: "Blog title 3", intro: "This is blog intro 3" },
-  ];
+app.get("/single-blog", async (req, res) => {
+  let blog = await Blog.findById("68d3ccc48a26cc71c33b540c");
+  res.json(blog);
+});
+
+app.get("/", async (req, res) => {
+  let blogs = await Blog.find().sort({ createdAt: -1 });
+  console.log(blogs);
   res.render("home", { blogs, title: "Home" });
 });
 
--- models/Blog.js
+++ models/Blog.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    intro: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
   },
-  intro: {
-    type: String,
-    required: true,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Blog = mongoose.model("Blog", blogSchema); // convention: singular and capitalized
 module.exports = Blog;

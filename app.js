5a5537f40625780b5681996740a0722ce6024da9
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-09-24 20:31:35 +0700

    Ep-33-Blog-Delete-Feature

--- app.js
+++ app.js
@@ -69,6 +69,17 @@ app.get("/blogs/create", (req, res) => {
   res.render("blogs/create", { title: "Blog Create" });
 });
 
+app.post("/blogs/:id/delete", async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    await Blog.findByIdAndDelete(id);
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
+    next();
+  }
+});
+
 app.get("/blogs/:id", async (req, res, next) => {
   try {
     let id = req.params.id;

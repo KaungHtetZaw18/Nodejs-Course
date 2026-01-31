6d0bab68c84da4632925ae44ea63037611e9db3a
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2026-02-01 05:17:44 +0700

    Ep-107-Delete-Recipe-Feature

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import axios from "axios";
 import Ingredients from "./Ingredients";
-export default function RecipeCard({ recipe }) {
+export default function RecipeCard({ recipe, onDeleted }) {
+  let deleteRecipe = async () => {
+    //api request
+    let res = await axios.delete(
+      "http://localhost:8000/api/recipes/" + recipe._id,
+    );
+    if (res.status === 200) {
+      onDeleted(recipe._id);
+    }
+  };
   return (
     <div className="bg-white p-5 rounded-2xl space-y-4">
-      <h3 className="text-xl font-bold text-orange-400">{recipe.title}</h3>
+      <div className="flex justify-between items-center">
+        <h3 className="text-xl font-bold text-orange-400">{recipe.title}</h3>
+        <button
+          className="bg-red-500 px-2 py-1 rounded-lg text-white text-sm"
+          onClick={deleteRecipe}
+        >
+          Delete
+        </button>
+      </div>
       <p>Description</p>
       <p>{recipe.description}</p>
       <Ingredients ingredients={recipe.ingredients} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import RecipeCard from "../components/RecipeCard";
 import Pagination from "../components/Pagination";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Home() {
   let [recipes, setRecipes] = useState([]);
   let [links, setLinks] = useState(null);
-
+  let navigate = useNavigate();
   let location = useLocation();
   let searchQuery = new URLSearchParams(location.search);
   let page = searchQuery.get("page"); // string
@@ -31,11 +31,19 @@ export default function Home() {
     fetchRecipes();
   }, [page]);
 
+  let onDeleted = (_id) => {
+    if (recipes.length === 1 && page > 1) {
+      navigate("/?page=" + (page - 1));
+    } else {
+      setRecipes((prev) => prev.filter((r) => r._id !== _id));
+    }
+  };
+
   return (
     <div className="space-y-3">
       {!!recipes.length &&
         recipes.map((recipe) => (
-          <RecipeCard recipe={recipe} key={recipe._id} />
+          <RecipeCard recipe={recipe} key={recipe._id} onDeleted={onDeleted} />
         ))}
       {!!links && <Pagination links={links} page={page || 1} />}
     </div>

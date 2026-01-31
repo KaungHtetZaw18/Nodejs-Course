6cafa7c03b87cf9bbd02c944598064ae32e2340d
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2026-02-01 01:48:30 +0700

    Ep-95-Minor-improvements-for-single-recipe-api

--- backend/controller/RecipeController.js
+++ backend/controller/RecipeController.js
@@ -1,5 +1,5 @@
 const Recipe = require("../models/Recipe");
-
+const mongoose = require("mongoose");
 const RecipeController = {
   index: async (req, res) => {
     let recipes = await Recipe.find().sort({ createdAt: -1 });
@@ -13,10 +13,16 @@ const RecipeController = {
   show: async (req, res) => {
     try {
       let id = req.params.id;
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ msg: "not a valid id" });
+      }
       let recipe = await Recipe.findById(id);
+      if (!recipe) {
+        return res.status(404).json({ msg: "recipe not found" });
+      }
       return res.json(recipe);
     } catch (e) {
-      return res.status(404).json({ msg: "recipe not found." });
+      return res.status(500).json({ msg: "internet server error" });
     }
   },
   destroy: (req, res) => {

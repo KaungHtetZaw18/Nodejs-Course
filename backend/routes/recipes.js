b2f799aec054fd42ded86b9bbd2f93ba1d75e90e
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2026-02-01 01:40:42 +0700

    Ep-94-using-express-validator

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const RecipeController = require("../controller/RecipeController");
+const { body } = require("express-validator");
+const handleErrorMessage = require("../middlewares/handleErrorMessage");
 const router = express.Router();
 
 router.get("", RecipeController.index);
-router.post("", RecipeController.store);
+router.post(
+  "",
+  [
+    body("title").notEmpty(),
+    body("description").notEmpty(),
+    body("ingredients").notEmpty().isArray({ min: 3 }),
+  ],
+  handleErrorMessage,
+  RecipeController.store,
+);
 router.get("/:id", RecipeController.show);
 router.delete("/:id", RecipeController.destroy);
 router.patch("/:id", RecipeController.update);

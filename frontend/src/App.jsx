26edec7bf0f172a96cc60c1ea441c84500210b4a
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2026-02-01 02:20:02 +0700

    Ep-98-react-router-and-layout-setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="text-blue-500 text-3xl">Hello World</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Outlet />
+      </div>
     </>
   );
 }

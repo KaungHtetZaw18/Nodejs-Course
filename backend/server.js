d5a064ad0730cf55d07039d4da6fab65942dcbd7
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2026-02-02 12:33:53 +0700

    Ep-112-Cookie-101

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,25 @@
 const express = require("express");
 require("dotenv").config();
 const morgan = require("morgan");
-const mongoose = require("mongoose");
 const recipesRoutes = require("./routes/recipes");
 const usersRoutes = require("./routes/users");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
 const app = express();
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to DB");
-    app.listen(process.env.PORT, () => {
-      console.log("App is running on localhost:" + process.env.PORT);
-    });
-  })
-  .catch((err) => {
-    console.error("DB Connection Error:", err);
+const mongoURL =
+  "mongodb+srv://hlaingminthan:test1234@mern-cluster.cut3lbf.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoURL).then(() => {
+  console.log("connected to db");
+  app.listen(process.env.PORT, () => {
+    console.log("app is running on localhost:" + process.env.PORT);
   });
-
-app.use(cors()); //local development --Warning--
+});
+app.use(cors()); //local development --WARNING---
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   return res.json({ hello: "world" });
@@ -29,3 +27,15 @@ app.get("/", (req, res) => {
 
 app.use("/api/recipes", recipesRoutes);
 app.use("/api/users", usersRoutes);
+
+app.get("/set-cookie", (req, res) => {
+  // res.setHeader('Set-Cookie','name=hlaingminthan');
+  res.cookie("name", "aungaung");
+  res.cookie("important-key", "value", { httpOnly: true });
+  return res.send("cookie already set");
+});
+
+app.get("/get-cookie", (req, res) => {
+  let cookies = req.cookies;
+  return res.json(cookies);
+});
